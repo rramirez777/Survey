5ddab9cddd5f41ef46e2d4a9609f0c119491ef78
rramirez777 <raramirezh@unal.edu.co>
2025-09-02 15:45:11 -0500

    Add files via upload

---
+++ Survey.py
@@ -0,0 +1,10 @@
+#Encuesta para 6 personas, nombre, carrera, idea de proyecto
+
+Encuesta = [
+    "¿Cual es tu nombre?",
+    "¿Que carrera estudias?",
+    "¿Cual es tu idea de proyecto?",
+]
+for i in range (6):
+    for E in Encuesta:
+        Respuesta= input(E + "")
